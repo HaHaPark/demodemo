746a3a2d125eec69c2a4821b69cf2b012302ca29
ParkHa <ugauga19892556@gmail.com>
2024-05-23 22:17:04 +0900

    데모임

---
+++ src/test/java/com/example/demodemo/DemodemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodemoApplicationTests {
+	//demo임!!
+
+	@Test
+	void contextLoads() {
+	}
+
+}
